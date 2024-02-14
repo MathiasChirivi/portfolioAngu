2c86c983440d195bcdecd985df86bb546e117738
MathiasChirivi <chirivimathias3gmail.com>
2024-02-14 16:00:32 +0100

    add component header and home for portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './components/app-header/app-header.component';
+import { AppMainComponent } from './components/app-main/app-main.component';
+import { AppHomeComponent } from './components/app-home/app-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppMainComponent,
+    AppHomeComponent
   ],
   imports: [
     BrowserModule,
